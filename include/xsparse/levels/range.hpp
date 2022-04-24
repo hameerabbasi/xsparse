1b7d921fed6f71f31fe4589cae1157c354e7cfff
Bharath <bharathkk0603@gmail.com>
2022-04-24 19:17:58 +0530

    Implemented range and offset levels (#4)
    Also add a test for the DIA format.

---
+++ include/xsparse/levels/range.hpp
@@ -0,0 +1,79 @@
+#ifndef XSPARSE_LEVELS_RANGE_HPP
+#define XSPARSE_LEVELS_RANGE_HPP
+
+#include <utility>
+#include <xsparse/util/base_traits.hpp>
+#include <xsparse/level_capabilities/coordinate_iterate.hpp>
+#include <tuple>
+
+namespace xsparse
+{
+    namespace levels
+    {
+        template <class LowerLevels, class IK, class PK, class OffsetContainer>
+        class range;
+
+        template <class... LowerLevels, class IK, class PK, class OffsetContainer>
+        class range<std::tuple<LowerLevels...>, IK, PK, OffsetContainer>
+            : public level_capabilities::coordinate_value_iterate<range,
+                                                                  std::tuple<LowerLevels...>,
+                                                                  IK,
+                                                                  PK,
+                                                                  OffsetContainer>
+        {
+            using BaseTraits
+                = util::base_traits<range, std::tuple<LowerLevels...>, IK, PK, OffsetContainer>;
+
+        public:
+            range(IK size_N, IK size_M)
+                : m_size_N(std::move(size_N))
+                , m_size_M(std::move(size_M))
+                , m_offset()
+            {
+            }
+
+            range(IK size_N, IK size_M, OffsetContainer const& offset)
+                : m_size_N(std::move(size_N))
+                , m_size_M(std::move(size_M))
+                , m_offset(offset)
+            {
+            }
+
+            range(IK size_N, IK size_M, OffsetContainer&& offset)
+                : m_size_N(std::move(size_N))
+                , m_size_M(std::move(size_M))
+                , m_offset(offset)
+            {
+            }
+
+            inline std::pair<IK, IK> coord_bounds(typename BaseTraits::I i) const noexcept
+            {
+                static_assert(std::tuple_size_v<decltype(i)> >= 1,
+                              "Tuple size should be at least 1");
+                return { static_cast<IK>(std::max(0, -m_offset[std::get<0>(i)])),
+                         static_cast<IK>(std::min(m_size_N, m_size_M - m_offset[std::get<0>(i)])) };
+            }
+
+            inline std::optional<PK> coord_access(typename BaseTraits::PKM1 pkm1,
+                                                  [[maybe_unused]] typename BaseTraits::I i,
+                                                  IK ik) const noexcept
+            {
+                return std::optional(static_cast<PK>(pkm1 * m_size_N + ik));
+            }
+
+        private:
+            IK m_size_N, m_size_M;
+            OffsetContainer m_offset;
+        };
+    }
+
+    template <class... LowerLevels, class IK, class PK, class OffsetContainer>
+    struct util::coordinate_position_trait<
+        levels::range<std::tuple<LowerLevels...>, IK, PK, OffsetContainer>>
+    {
+        using Coordinate = IK;
+        using Position = PK;
+    };
+}
+
+#endif  // XSPARSE_LEVELS_RANGE_HPP
---
+++ test/source/range_test.cpp
@@ -0,0 +1,53 @@
+#include <doctest/doctest.h>
+
+#include <tuple>
+#include <vector>
+
+#include <xsparse/levels/range.hpp>
+#include <xsparse/levels/dense.hpp>
+#include <xsparse/levels/offset.hpp>
+#include <xsparse/version.h>
+
+#include <iostream>
+
+TEST_CASE("Range-DIA")
+{
+    constexpr uintptr_t SIZE = 4;
+    constexpr uintptr_t SIZE_N = 4;
+    constexpr uintptr_t SIZE_M = 6;
+    constexpr uint8_t ZERO = 0;
+
+    std::vector<int16_t> const offset{ -3, -1, 0, 1 };
+
+    xsparse::levels::dense<std::tuple<>, uintptr_t, uintptr_t> d{ SIZE };
+
+    xsparse::levels::range<std::tuple<decltype(d)>, uintptr_t, uintptr_t, std::vector<int16_t>> r{
+        SIZE_N, SIZE_M, offset
+    };
+    xsparse::levels::
+        offset<std::tuple<decltype(r), decltype(d)>, uintptr_t, uintptr_t, std::vector<int16_t>>
+            o{ SIZE, offset };
+
+    uintptr_t l1 = 0;
+
+    for (auto const [i1, p1] : d.iter_helper(std::make_tuple(), ZERO))
+    {
+        CHECK(l1 == p1);
+        CHECK(l1 == i1);
+        uintptr_t l2 = std::max(0, -offset[l1]);
+        for (auto const [i2, p2] : r.iter_helper(std::make_tuple(i1), p1))
+        {
+            CHECK(l2 + SIZE_N * l1 == p2);
+            CHECK(l2 == i2);
+            for (auto const [i3, p3] : o.iter_helper(std::make_tuple(i2, i1), p2))
+            {
+                CHECK(p2 == p3);
+                CHECK(l2 + offset[l1] == i3);
+            }
+            ++l2;
+        }
+        CHECK(l2 == std::min(SIZE_N, SIZE_M - offset[l1]));
+        ++l1;
+    }
+    CHECK(l1 == SIZE);
+}
