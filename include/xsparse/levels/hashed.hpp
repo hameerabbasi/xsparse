ce8055694da3047ca55c0ede5de40827dba8d2d7
Bharath <bharathkk.72@gmail.com>
2022-06-29 17:13:34 +0530

    added hashed level + tests (#6)

---
+++ include/xsparse/levels/hashed.hpp
@@ -0,0 +1,167 @@
+#ifndef XSPARSE_LEVELS_HASHED_HPP
+#define XSPARSE_LEVELS_HASHED_HPP
+
+#include <tuple>
+#include <utility>
+#include <optional>
+#include <unordered_map>
+
+#include <xsparse/util/base_traits.hpp>
+#include <xtl/xiterator_base.hpp>
+
+namespace xsparse
+{
+    namespace levels
+    {
+        template <class LowerLevels, class IK, class PK, class CrdContainer>
+        class hashed;
+
+        template <class... LowerLevels, class IK, class PK, class CrdContainer>
+        class hashed<std::tuple<LowerLevels...>, IK, PK, CrdContainer>
+        {
+            using BaseTraits
+                = util::base_traits<hashed, std::tuple<LowerLevels...>, IK, PK, CrdContainer>;
+
+        public:
+            class iteration_helper
+            {
+                static_assert(std::is_nothrow_invocable_r_v<std::optional<typename BaseTraits::PK>,
+                                                            decltype(&BaseTraits::Level::locate),
+                                                            typename BaseTraits::Level&,
+                                                            typename BaseTraits::PKM1,
+                                                            typename BaseTraits::IK>);
+
+            private:
+                std::unordered_map<typename BaseTraits::IK, typename BaseTraits::PK>& m_map;
+
+            public:
+                class iterator;
+                using value_type =
+                    typename std::pair<typename BaseTraits::IK, typename BaseTraits::PK>;
+                using difference_type = typename std::make_signed_t<typename BaseTraits::PK>;
+                using pointer =
+                    typename std::pair<typename BaseTraits::IK, typename BaseTraits::PK>*;
+                using reference = std::pair<typename BaseTraits::IK, typename BaseTraits::PK>;
+                using iterator_type = iterator;
+
+                class iterator : public xtl::xbidirectional_iterator_base2<iteration_helper>
+                {
+                private:
+                    using wrapped_iterator_type =
+                        typename std::unordered_map<typename BaseTraits::IK,
+                                                    typename BaseTraits::PK>::const_iterator;
+                    wrapped_iterator_type wrapped_it;
+
+                public:
+                    explicit inline iterator(wrapped_iterator_type wrapped) noexcept
+                        : wrapped_it(wrapped)
+                    {
+                    }
+
+                    inline std::tuple<typename BaseTraits::IK, typename BaseTraits::PK> operator*()
+                        const noexcept
+                    {
+                        return { wrapped_it->first, wrapped_it->second };
+                    }
+
+                    inline bool operator==(const iterator& other) const noexcept
+                    {
+                        return wrapped_it == other.wrapped_it;
+                    }
+
+                    inline iterator& operator++() noexcept
+                    {
+                        ++wrapped_it;
+                        return *this;
+                    }
+
+                    inline iterator& operator--() noexcept
+                    {
+                        --wrapped_it;
+                        return *this;
+                    }
+                };
+
+                explicit inline iteration_helper(
+                    std::unordered_map<typename BaseTraits::IK, typename BaseTraits::PK>&
+                        map) noexcept
+                    : m_map(map)
+                {
+                }
+
+                inline iterator_type begin() const noexcept
+                {
+                    return iterator_type{ m_map.begin() };
+                }
+
+                inline iterator_type end() const noexcept
+                {
+                    return iterator_type{ m_map.end() };
+                }
+            };
+
+            iteration_helper iter_helper(typename BaseTraits::PKM1 pkm1)
+            {
+                return iteration_helper{ this->m_crd[pkm1] };
+            }
+
+            hashed(IK size, PK crd_size)
+                : m_size(std::move(size))
+                , m_crd_size(std::move(crd_size))
+                , m_crd()
+            {
+            }
+
+            hashed(IK size, PK crd_size, CrdContainer const& crd)
+                : m_size(std::move(size))
+                , m_crd_size(std::move(crd_size))
+                , m_crd(crd)
+            {
+            }
+
+            hashed(IK size, PK crd_size, CrdContainer&& crd)
+                : m_size(std::move(size))
+                , m_crd_size(std::move(crd_size))
+                , m_crd(crd)
+            {
+            }
+
+            inline auto locate(typename BaseTraits::PKM1 pkm1, IK ik) const noexcept
+            {
+                auto it = m_crd[pkm1].find(ik);
+                return it != m_crd[pkm1].end() ? std::optional<PK>(it->second) : std::nullopt;
+            }
+
+            inline void insert_init(typename BaseTraits::IK szkm1) const noexcept
+            {
+                m_crd.resize(szkm1);
+            }
+
+            inline void insert_coord(typename BaseTraits::PKM1 pkm1, PK pk, IK ik) const noexcept
+            {
+                m_crd[pkm1][ik] = pk;
+            }
+
+            inline void insert_edges(typename BaseTraits::PKM1 pkm1) const noexcept
+            {
+                m_crd_size += m_crd[pkm1].size();
+            }
+
+        private:
+            IK m_size;
+            PK m_crd_size;
+            CrdContainer m_crd;
+        };
+    }  // namespace levels
+
+    template <class... LowerLevels, class IK, class PK, class CrdContainer>
+    struct util::coordinate_position_trait<
+        levels::hashed<std::tuple<LowerLevels...>, IK, PK, CrdContainer>>
+    {
+        using Coordinate = IK;
+        using Position = PK;
+    };
+}  // namespace xsparse
+
+
+#endif
---
+++ test/source/hashed_test.cpp
@@ -0,0 +1,178 @@
+#include <doctest/doctest.h>
+#include <xsparse/levels/hashed.hpp>
+#include <xsparse/levels/dense.hpp>
+#include <xsparse/levels/singleton.hpp>
+
+TEST_CASE("Dense-Hashed")
+{
+    constexpr uintptr_t SIZE0 = 3;
+    constexpr uint8_t ZERO = 0;
+    constexpr uintptr_t SIZE1 = 3;
+    constexpr uintptr_t SIZE2 = 7;
+
+    std::unordered_map<uintptr_t, uintptr_t> const umap1{ { 5, 2 }, { 6, 1 }, { 4, 0 } };
+    std::unordered_map<uintptr_t, uintptr_t> const umap2{ { 2, 5 } };
+    std::unordered_map<uintptr_t, uintptr_t> const umap3{ { 3, 3 }, { 1, 4 }, { 0, 6 } };
+
+    std::vector<std::unordered_map<uintptr_t, uintptr_t>> const crd{ umap1, umap2, umap3 };
+
+    xsparse::levels::dense<std::tuple<>, uintptr_t, uintptr_t> d{ SIZE0 };
+
+    xsparse::levels::hashed<std::tuple<decltype(d)>,
+                            uintptr_t,
+                            uintptr_t,
+                            std::vector<std::unordered_map<uintptr_t, uintptr_t>>>
+        h{ SIZE1, SIZE2, crd };
+
+    uintptr_t l1 = 0;
+    for (auto const [i1, p1] : d.iter_helper(std::make_tuple(), ZERO))
+    {
+        CHECK(l1 == i1);
+        CHECK(l1 == p1);
+        uintptr_t l2 = 0;
+        for (auto const [i2, p2] : h.iter_helper(p1))
+        {
+            CHECK(crd[l1].at(i2) == p2);
+            ++l2;
+        }
+        CHECK(l2 == crd[l1].size());
+        ++l1;
+    }
+    CHECK(l1 == SIZE0);
+}
+
+TEST_CASE("Hashed-Hashed")
+{
+    constexpr uintptr_t SIZE0 = 1;
+    constexpr uintptr_t SIZE1 = 3;
+    constexpr uint8_t ZERO = 0;
+    constexpr uintptr_t SIZE2 = 3;
+    constexpr uintptr_t SIZE3 = 7;
+
+    std::unordered_map<uintptr_t, uintptr_t> const umap1{ { 0, 1 }, { 2, 0 }, { 1, 2 } };
+
+    std::unordered_map<uintptr_t, uintptr_t> const umap2{ { 5, 2 }, { 6, 1 }, { 4, 0 } };
+    std::unordered_map<uintptr_t, uintptr_t> const umap3{ { 2, 5 } };
+    std::unordered_map<uintptr_t, uintptr_t> const umap4{ { 3, 3 }, { 1, 4 }, { 0, 6 } };
+
+    std::vector<std::unordered_map<uintptr_t, uintptr_t>> const crd0{ umap1 };
+
+    std::vector<std::unordered_map<uintptr_t, uintptr_t>> const crd1{ umap2, umap3, umap4 };
+
+    xsparse::levels::hashed<std::tuple<>,
+                            uintptr_t,
+                            uintptr_t,
+                            std::vector<std::unordered_map<uintptr_t, uintptr_t>>>
+        h{ SIZE0, SIZE1, crd0 };
+
+    xsparse::levels::hashed<std::tuple<decltype(h)>,
+                            uintptr_t,
+                            uintptr_t,
+                            std::vector<std::unordered_map<uintptr_t, uintptr_t>>>
+        h1{ SIZE2, SIZE3, crd1 };
+
+    uintptr_t l1 = 0;
+    for (auto const [i1, p1] : h.iter_helper(ZERO))
+    {
+        CHECK(crd0[ZERO].at(i1) == p1);
+        uintptr_t l2 = 0;
+        for (auto const [i2, p2] : h1.iter_helper(p1))
+        {
+            CHECK(crd1[p1].at(i2) == p2);
+            ++l2;
+        }
+        CHECK(l2 == crd1[p1].size());
+        ++l1;
+    }
+    CHECK(l1 == crd0[ZERO].size());
+}
+
+TEST_CASE("Hashed-Singleton")
+{
+    constexpr uintptr_t SIZE0 = 1;
+    constexpr uintptr_t SIZE1 = 3;
+    constexpr uintptr_t SIZE2 = 3;
+    constexpr uint8_t ZERO = 0;
+
+    std::unordered_map<uintptr_t, uintptr_t> const umap1{ { 0, 1 }, { 2, 0 }, { 1, 2 } };
+
+    std::vector<std::unordered_map<uintptr_t, uintptr_t>> const crd0{ umap1 };
+
+    std::vector<uintptr_t> const crd1{ 0, 3, 4 };
+
+    xsparse::levels::hashed<std::tuple<>,
+                            uintptr_t,
+                            uintptr_t,
+                            std::vector<std::unordered_map<uintptr_t, uintptr_t>>>
+        h{ SIZE0, SIZE1, crd0 };
+
+    xsparse::levels::
+        singleton<std::tuple<decltype(h)>, uintptr_t, uintptr_t, std::vector<uintptr_t>>
+            s{ SIZE2, crd1 };
+
+    uintptr_t l1 = 0;
+    for (auto const [i1, p1] : h.iter_helper(ZERO))
+    {
+        CHECK(crd0[ZERO].at(i1) == p1);
+        uintptr_t l2 = p1;
+        for (auto const [i2, p2] : s.iter_helper(std::make_tuple(i1), p1))
+        {
+            CHECK(l2 == p2);
+            CHECK(crd1[l2] == i2);
+            ++l2;
+        }
+        CHECK(l2 == p1 + 1);
+        ++l1;
+    }
+    CHECK(l1 == crd0[ZERO].size());
+}
+
+TEST_CASE("Hashed-Singleton-Dense")
+{
+    constexpr uintptr_t SIZE0 = 1;
+    constexpr uintptr_t SIZE1 = 5;
+    constexpr uintptr_t SIZE2 = 1;
+    constexpr uintptr_t SIZE3 = 2;
+    constexpr uintptr_t ZERO = 0;
+
+    std::unordered_map<uintptr_t, uintptr_t> umap1{
+        { 1, 0 }, { 0, 2 }, { 2, 1 }, { 3, 4 }, { 4, 3 }
+    };
+
+    std::vector<std::unordered_map<uintptr_t, uintptr_t>> const crd0{ umap1 };
+    std::vector<uintptr_t> const crd1 = { 0, 2, 0, 2, 3 };
+
+    xsparse::levels::hashed<std::tuple<>,
+                            uintptr_t,
+                            uintptr_t,
+                            std::vector<std::unordered_map<uintptr_t, uintptr_t>>>
+        h{ SIZE0, SIZE1, crd0 };
+    xsparse::levels::
+        singleton<std::tuple<decltype(h)>, uintptr_t, uintptr_t, std::vector<uintptr_t>>
+            s{ SIZE2, crd1 };
+    xsparse::levels::dense<std::tuple<decltype(s)>, uintptr_t, uintptr_t> d{ SIZE3 };
+
+    uintptr_t l1 = 0;
+    for (auto const [i1, p1] : h.iter_helper(ZERO))
+    {
+        CHECK(crd0[ZERO].at(i1) == p1);
+        uintptr_t l2 = p1;
+        for (auto const [i2, p2] : s.iter_helper(std::make_tuple(i1), p1))
+        {
+            CHECK(l2 == p2);
+            CHECK(crd1[l2] == i2);
+            uintptr_t l3 = 0;
+            for (auto const [i3, p3] : d.iter_helper(std::make_tuple(i2), p2))
+            {
+                CHECK(l3 == i3);
+                CHECK(l3 + SIZE3 * l2 == p3);
+                ++l3;
+            }
+            CHECK(l3 == SIZE3);
+            ++l2;
+        }
+        CHECK(l2 == p1 + 1);
+        ++l1;
+    }
+    CHECK(l1 == crd0[ZERO].size());
+}
