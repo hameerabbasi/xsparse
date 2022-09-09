3b1c0c5e0d6b634cd5eacc7902a01abe68f4307d
Bharath <bharathkk.72@gmail.com>
2022-09-09 16:38:25 +0530

    added coiteration iterator

--- include/xsparse/level_capabilities/co_iteration.hpp
+++ include/xsparse/level_capabilities/co_iteration.hpp
@@ -1,40 +1,125 @@
 #ifndef XSPARSE_CO_ITERATION_HPP
 #define XSPARSE_CO_ITERATION_HPP
 
-#include "../levels/compressed.hpp"
 #include <vector>
+#include <tuple>
 
 namespace xsparse::level_capabilities
 {
-    template <class T, class U, class IK, class PK>
-    T merge(T t, U u)
+    template <class IK, class PK, class Levels, class IterHelpers>
+    class Coiterate;
+
+    template <class IK, class PK, class... Levels, class... IterHelpers>
+    class Coiterate<IK, PK, std::tuple<Levels...>, std::tuple<IterHelpers...>>
     {
-        uintptr_t SIZE = 100;
-        std::vector<PK> pos;
-        std::vector<IK> crd;
-        auto cur = -1;
-        int l1 = 0;
-        for (auto const [i, p] : t.iter_helper(std::make_tuple(), 0))
+    public:
+        class coiteration_helper
         {
-            for (auto const [i1, p1] : u.iter_helper(std::make_tuple(i), p))
+        private:
+            std::tuple<Levels...> levelsTuple;
+            std::tuple<IterHelpers...> iterHelpers;
+
+        public:
+            explicit inline coiteration_helper(std::tuple<IterHelpers...> iter_helpers,
+                                               Levels... levels)
+                : levelsTuple(std::make_tuple(levels...))
+                , iterHelpers(iter_helpers)
             {
-                crd.push_back(i1);
             }
-            if (i != cur)
+
+            class iterator
             {
-                pos.push_back(l1);
-                for (auto k = 0; k < i - cur - 1; ++k)
+            private:
+                coiteration_helper const& m_coiterHelper;
+                std::tuple<IterHelpers::template iterator...> iterators;
+                IK min;
+
+            public:
+                using iterator_category = std::forward_iterator_tag;
+                using key_type = IK;
+                using pointer = std::pair<IK, std::vector<std::optional<PK>>>*;
+                using reference = std::tuple<IK, std::vector<std::optional<PK>>>;
+
+                explicit inline iterator(coiteration_helper const& coiterHelper,
+                                         std::tuple<IterHelpers::template iterator...> it)
+                    : m_coiterHelper(coiterHelper)
+                    , iterators(it)
                 {
-                    pos.push_back(l1);
+                    min = static_cast<IK>(std::get<0>(*std::get<0>(iterators)));
+                    std::apply([&](auto&&... args)
+                               { ((min = std::min(min, std::get<0>(*args))), ...); },
+                               iterators);
                 }
-                cur = i;
+
+                auto get_PKs()
+                {
+                    std::vector<std::optional<PK>> vec;
+                    std::apply([&](const auto&... args) { (vec.push_back(*locate(args)), ...); },
+                               iterators);
+                    return vec;
+                }
+
+                template <class iter>
+                auto locate(iter i)
+                {
+                    return (std::get<0>(*i) == min) ? std::optional<PK>(std::get<1>(*i))
+                                                    : std::nullopt;
+                }
+
+                template <class iter>
+                void advance_iter(iter& i)
+                {
+                    if (std::get<0>(*i) == min)
+                    {
+                        i = ++i;
+                    }
+                }
+
+                reference operator*() noexcept
+                {
+                    std::apply([&](auto&&... args)
+                               { ((min = std::min(min, std::get<0>(*args))), ...); },
+                               iterators);
+                    auto PK_tuple = get_PKs();
+                    return { min, PK_tuple };
+                }
+
+                iterator& operator++()
+                {
+                    std::apply([&](auto&... args) { ((advance_iter(args)), ...); }, iterators);
+                    min = static_cast<IK>(std::get<0>(*std::get<0>(iterators)));
+                    std::apply([&](auto&&... args)
+                               { ((min = std::min(min, std::get<0>(*args))), ...); },
+                               iterators);
+                    return *this;
+                }
+
+                /*friend bool operator==(const iterator& a, const iterator& b){
+
+                };
+
+                friend bool operator!=(const iterator& a, const iterator& b){
+
+                };*/
+            };
+
+            inline iterator begin() const noexcept
+            {
+                auto f = [](const auto&... args) { return std::tuple(args.begin()...); };
+                return iterator{ *this, std::apply(f, this->iterHelpers) };
             }
-            ++l1;
+
+            inline iterator end() const noexcept
+            {
+                auto f = [](const auto&... args) { return std::tuple(args.end()...); };
+                return iterator{ *this, std::apply(f, this->iterHelpers) };
+            }
+        };
+
+        coiteration_helper coiter_helper(std::tuple<IterHelpers...> ih, Levels... levels)
+        {
+            return coiteration_helper{ ih, levels... };
         }
-        pos.push_back(l1);
-        T c{ SIZE, pos, crd };
-        return c;
-    }
-}
+    };
 
 #endif
