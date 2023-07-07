f591cc445bf1d4d0475d5bb292024baf94479ecc
Adam Li <adam2392@gmail.com>
2023-07-06 23:31:39 -0400

    Starting PR for checking compile-time
    Signed-off-by: Adam Li <adam2392@gmail.com>

--- include/xsparse/level_capabilities/co_iteration.hpp
+++ include/xsparse/level_capabilities/co_iteration.hpp
@@ -58,6 +58,20 @@ namespace xsparse::level_capabilities
             {
                 throw std::invalid_argument("level sizes should be same");
             }
+
+            // first check that the levels are all ordered or have locate
+            all_ordered_or_have_locate(std::make_index_sequence<std::tuple_size_v<decltype(m_levelsTuple)>>{});
+        }
+
+        // Helper function to check if a level is ordered
+        template <typename Level>
+        constexpr bool is_level_ordered(Level& level) {
+            return Level::LevelProperties::is_ordered;
+        }
+
+        template <std::size_t... I>
+        inline constexpr bool all_ordered_or_have_locate([[maybe_unused]] std::index_sequence<I...> i) {
+            return (true && ... && (is_level_ordered(std::get<I>(m_levelsTuple)) || has_locate_v<std::get<I>(m_levelsTuple)>)... );
         }
 
     public:
