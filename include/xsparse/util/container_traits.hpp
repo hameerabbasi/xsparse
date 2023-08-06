10b91002e246a16d2e14db8495faafa3774d383e
Adam Li <adam2392@gmail.com>
2023-08-06 16:24:46 -0400

    Adding required member functions of abstract container traits (#29)

--- include/xsparse/util/container_traits.hpp
+++ include/xsparse/util/container_traits.hpp
@@ -22,6 +22,33 @@ namespace xsparse::util
 
         template <class Key, class Val>
         using Map = TMap<Key, Val>;
+
+        // Check that the container traits have the properly defined methods and method signatures
+        // uses sample type inputs to check the signatures
+        static_assert(
+            std::is_same_v<decltype(std::declval<Vec<double>>().push_back(std::declval<double>())),
+                           void>,
+            "Vec must have `push_back` method with the correct signature.");
+
+        static_assert(std::is_same_v<decltype(std::declval<Vec<double>>().resize(
+                                         std::declval<typename Vec<double>::size_type>())),
+                                     void>,
+                      "Vec must have `resize` method with the correct signature.");
+
+        static_assert(
+            std::is_same_v<decltype(std::declval<Vec<double>>()[std::declval<std::size_t>()]),
+                           decltype(std::declval<Vec<double>>().at(std::declval<std::size_t>()))>,
+            "Vec must have `operator[]` method with the correct signature.");
+
+        static_assert(std::is_same_v<
+                          decltype(std::declval<Map<int, std::size_t>>().find(std::declval<int>())),
+                          typename Map<int, std::size_t>::iterator>,
+                      "Map must have `find` method with the correct signature.");
+
+        static_assert(std::is_same_v<decltype(std::declval<Set<std::size_t>>().contains(
+                                         std::declval<std::size_t>())),
+                                     bool>,
+                      "Set must have `contains` method with the correct signature.");
     };
 }
 
